600231948d14b5d1aa3d7671a0ffd8c08d0f9714
ReoIto0210 <ito.reo@temona.co.jp>
2023-06-21 10:05:24 +0900

    settings.pyをローカル環境用と本番環境用に分割する

---
+++ unityapi/settings/production.py
@@ -0,0 +1,45 @@
+from .base import *
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'production': {
+            'format': '%(asctime)s [%(levelname)s] %(process)d %(thread)d '
+                      '%(pathname)s:%(lineno)d %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/django/production.log',
+            'formatter': 'production',
+            'level': 'INFO',
+        },
+    },
+    'loggers': {
+        # 自作したログ出力
+        '': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        # Djangoの警告・エラー
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+    'handlers': {
+        'file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': '/var/log/django/production.log',
+            'formatter': 'production',
+            'maxBytes': 1024 * 1024 * 100,  # 100MB
+            'backupCount': 5,
+        },
+    },
+}
+
